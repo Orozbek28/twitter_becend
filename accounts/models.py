0a63f672b3bc1338f86c291e3e470c3bf270f6cb
Orozbek28 <gulmuradovorozbek297@gmail.com>
2023-05-05 10:57:43 +0600

    forst_commit

---
+++ accounts/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    profile_avatar = models.ImageField(default='profile/default-avatar.png', upload_to='profile')
+
+    def __str__(self):
+        return self.username
+
+
+class Table(models.Model):
+    smil_serial = models.SmallAutoField(primary_key=True)
+    # serial = models.AutoField()
+    # big_serial = models.BigAutoField()
+    smail_integer = models.SmallIntegerField()
+    big_integer = models.BigIntegerField()
+    numeric = models.FloatField()
